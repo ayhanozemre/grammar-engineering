bdbb9180017af6a0accdbc5e2968cbf128ba88c3
marijanedjalkova <nedyalkova.marya@gmail.com>
2017-03-19 21:20:58 +0000

    test method, but ambigious parsing

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 from nltk import *
 cfg = CFG.fromstring("""
-	S -> NP | NP VP | Question
-	Question -> QuestionWord Auxiliary NP VP
+	S -> NP | Action | Question
+	Question -> QuestionWord Auxiliary Action
+	Action -> NP VP | NP RB VP
 	QuestionWord -> WRB
 	Auxiliary -> 'does' | 'do' | 'did'
 	NP -> NN | DT Noun | NN CC NN 
 	NN -> ProperNoun | Noun
 	ProperNoun -> 'Gromit' | 'Wallace'
 	Noun -> 'cheese' | 'water' | 'kitchen' | 'dinner'
-	VP -> V | V DT Noun | V NN
+	VP -> V | V DT Noun | V NN | V NN NN | V NP | VP CC VP
 	V -> VBZ | VB | VBD | VBG
 	VBZ -> 'barks' | 'laughs' | 'eats' | 'feeds' | 'thinks' | 'drinks' | 'does'
 	VB -> 'bark' | 'laugh' | 'eat' | 'feed' | 'think' | 'drink' | 'do'
@@ -23,24 +24,32 @@ cfg = CFG.fromstring("""
 	""")
 cfparser = ChartParser(cfg)
 text = """\
+Wallace thinks Gromit eats cheese and drinks water
+Wallace often eats tasty soft cheese in the kitchen after dinner
+when Gromit barks Wallace feeds Gromit
+"""
+
+done = """\
 Gromit barks
 Gromit barked
 Wallace and Gromit eat cheese
 Wallace and Gromit ate cheese
 Wallace feeds Gromit
 Wallace seldom feeds Gromit cheese
-Wallace thinks Gromit eats cheese and drinks water
-Wallace often eats tasty soft cheese in the kitchen after dinner
-when Gromit barks Wallace feeds Gromit
 when does Wallace eat cheese
 """
 
-sents = text.splitlines()
-counter = 0
-for sent in sents:
-	parses = cfparser.parse(sent.split())
-	for tree in parses:
-		print tree
-		print "--------------"
-		counter += 1
-print counter, "/", len(sents)
+
+def test(text):
+	sents = text.splitlines()
+	counter = 0
+	for sent in sents:
+		parses = cfparser.parse(sent.split())
+		for tree in parses:
+			print tree
+			print "--------------"
+			counter += 1
+	print counter, "/", len(sents)
+
+test(text)
+test(done)
