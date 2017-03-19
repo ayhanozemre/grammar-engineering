e1298778b34daea4c8d5375df189b4b67a50b77d
marijanedjalkova <nedyalkova.marya@gmail.com>
2017-03-19 20:16:31 +0000

    some more words

--- main.py
+++ main.py
@@ -3,13 +3,19 @@ cfg = CFG.fromstring("""
 	S -> NP VP
 	NP -> ProperNoun | Noun
 	ProperNoun -> 'Gromit' | 'Wallace'
-	Noun -> 'cheese' | 'water' | 'kitchen'
+	Noun -> 'cheese' | 'water' | 'kitchen' | 'dinner'
 	VP -> V
 	V -> VB | VBD | VBG | VBZ
-	VB -> 'bark' | 'laugh'
-	VBD -> 'barked' | 'laughed'
-	VBG -> 'barking' | 'laughing'
-	VBZ -> 'barks' | 'laughs'
+	VB -> 'bark' | 'laugh' | 'eat' | 'feed' | 'think' | 'drink' 
+	VBD -> 'barked' | 'laughed' | 'ate' | 'fed' | 'thought' | 'drank' 
+	VBG -> 'barking' | 'laughing' | 'eating' | 'feeding' | 'thinking' | 'drinking' 
+	VBZ -> 'barks' | 'laughs' | 'eats' | 'feeds' | 'thinks' | 'drinks' 
+	DT -> 'a' | 'the' 
+	IN -> 'in' | 'on' | 'at' | 'after' | 'when'
+	JJ -> 'tasty' | 'soft' 
+	CC -> 'and' | 'but'
+	RB -> 'seldom' | 'often' | 'when'
+	WP -> 'when'
 	""")
 cfparser = ChartParser(cfg)
 text = """\
