f889c1f38e6fb4bf65c2f80436066429d1bdb9c6
marijanedjalkova <nedyalkova.marya@gmail.com>
2017-03-19 20:17:37 +0000

    removing for now, but will return later

--- main.py
+++ main.py
@@ -5,10 +5,7 @@ cfg = CFG.fromstring("""
 	ProperNoun -> 'Gromit' | 'Wallace'
 	Noun -> 'cheese' | 'water' | 'kitchen' | 'dinner'
 	VP -> V
-	V -> VB | VBD | VBG | VBZ
-	VB -> 'bark' | 'laugh' | 'eat' | 'feed' | 'think' | 'drink' 
-	VBD -> 'barked' | 'laughed' | 'ate' | 'fed' | 'thought' | 'drank' 
-	VBG -> 'barking' | 'laughing' | 'eating' | 'feeding' | 'thinking' | 'drinking' 
+	V -> VBZ
 	VBZ -> 'barks' | 'laughs' | 'eats' | 'feeds' | 'thinks' | 'drinks' 
 	DT -> 'a' | 'the' 
 	IN -> 'in' | 'on' | 'at' | 'after' | 'when'
