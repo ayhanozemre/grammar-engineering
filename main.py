5d2fe9562fbb0ed5ac63eb0bd137bdb0280bf3af
marijanedjalkova <nedyalkova.marya@gmail.com>
2017-03-20 22:05:29 +0000

    subcategorization left to do

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ cfg_str ="""\
 	# Grammar
 
 	S -> NP[NUM=?n, PER=?p] VP[NUM=?n, PER=?p] | NP[NUM=?n, PER=?p] RB VP[NUM=?n, PER=?p] | Question[NUM=?n] | NP[NUM=?n, PER=?p] V[NUM=?n, PER=?p] S | IN S 
-	Question[NUM=?n] -> WhP Auxiliary[NUM=?n] NP[NUM=?n] VP
+	Question[NUM=?n] -> WhP Auxiliary[NUM=?n] NP[NUM=?n] VP[NUM=pl]
 	WhP -> WRB
 	NP[NUM=pl] -> NP[NUM=?n] CC NP[NUM=?n]
 	NP[NUM=?n, PER=?p] -> DT[NUM=?n] Nominal[NUM=?n] | Nominal[NUM=?n] | ProperNoun[NUM=?n, PER=?p] | Pronoun[NUM=?n, PER=?p] | AP NP[NUM=?n] | NP[NUM=?n] PP
@@ -18,12 +18,13 @@ cfg_str ="""\
 	ARG[CAT=np] -> NP
 	ARG[CAT=vp] -> VP
 	# Words
-
+	
 	V[NUM=sg, PER=3] -> 'barks' | 'laughs' | 'eats' | 'feeds' | 'thinks' | 'drinks' | 'does'
  	V[TENSE=past] -> 'barked' | 'laughed' | 'ate' | 'fed' | 'thought' | 'drank' | 'did'
  	V[TENSE=prespart] -> 'barking' | 'laughing' | 'eating' | 'feeding' | 'thinking' | 'drinking' | 'doing'
  	V[NUM=pl] -> 'bark' | 'laugh' | 'eat' | 'feed' | 'think' | 'drink' | 'do'
- 	Auxiliary -> 'does' | 'do' | 'did'
+ 	Auxiliary[NUM=sg, PER=3] -> 'does'
+ 	Auxiliary[NUM=pl] -> 'do' | 'did'
  	ProperNoun[NUM=sg, PER=3] -> 'Gromit' | 'Wallace'
  	Pronoun[PER=1] -> 'I' | 'we'
  	Pronoun[PER=2] -> 'you'
@@ -54,7 +55,7 @@ when does Wallace eat cheese
 
 invalid = """\
 Gromit bark
-when do Gromit eats cheese
+when do Gromit eat cheese
 Gromit barks the kitchen
 """
 
