f2b27906d8b03daaf45bb29af03db681bdfba31b
marijanedjalkova <nedyalkova.marya@gmail.com>
2017-03-21 15:38:27 +0000

    subcategorization works for all but one sentence

--- main.py
+++ main.py
@@ -4,25 +4,45 @@ cfg_str ="""\
 
 	# Grammar
 
-	S -> NP[NUM=?n, PER=?p] VP[NUM=?n, PER=?p] | NP[NUM=?n, PER=?p] RB VP[NUM=?n, PER=?p] | Question[NUM=?n] | NP[NUM=?n, PER=?p] V[NUM=?n, PER=?p] S | IN S 
-	Question[NUM=?n] -> WhP Auxiliary[NUM=?n] NP[NUM=?n] VP[NUM=pl]
+	S -> NP[NUM=?n, PER=?p] VP[NUM=?n, PER=?p, SUBCAT=?s] 
+	S -> NP[NUM=?n, PER=?p] RB VP[NUM=?n, PER=?p, SUBCAT=?s] 
+	S -> Question[NUM=?n] 
+	#S -> NP[NUM=?n, PER=?p] V[NUM=?n, PER=?p, SUBCAT=?s] S 
+	S -> IN[SUBCAT=[HEAD=vp, TAIL=[HEAD=pp, TAIL=nil]]]
+	#S -> IN NP[NUM=?n, PER=?p] VP[NUM=?n, PER=?p, SUBCAT=?s] NP[NUM=?n, PER=?p] VP[NUM=?n, PER=?p, SUBCAT=?s] 
+	Question[NUM=?n] -> WhP Auxiliary[NUM=?n] NP[NUM=?n] VP[NUM=pl, SUBCAT=?s]
 	WhP -> WRB
 	NP[NUM=pl] -> NP[NUM=?n] CC NP[NUM=?n]
 	NP[NUM=?n, PER=?p] -> DT[NUM=?n] Nominal[NUM=?n] | Nominal[NUM=?n] | ProperNoun[NUM=?n, PER=?p] | Pronoun[NUM=?n, PER=?p] | AP NP[NUM=?n] | NP[NUM=?n] PP
-	VP[NUM=?n, TENSE=?t, PER=?p] -> V[NUM=?n, TENSE=?t, PER=?p]| V[NUM=?n, TENSE=?t, PER=?p] NP NP | V[NUM=?n, TENSE=?t, PER=?p] NP 
-	VP[NUM=?n, TENSE=?t, PER=?p] -> VP[NUM=?n, TENSE=?t, PER=?p] CC VP[NUM=?n, TENSE=?t, PER=?p] | V[NUM=?n, TENSE=?t, PER=?p] PP  
+
+	# VP[NUM=?n, TENSE=?t, PER=?p] -> V[NUM=?n, TENSE=?t, PER=?p]
+	#VP[NUM=?n, TENSE=?t, PER=?p] -> V[NUM=?n, TENSE=?t, PER=?p] NP NP 
+	#VP[NUM=?n, TENSE=?t, PER=?p] -> V[NUM=?n, TENSE=?t, PER=?p] NP 
+	VP[NUM=?n, TENSE=?t, PER=?p, SUBCAT=?rest] -> VP[NUM=?n, TENSE=?t, PER=?p, SUBCAT=?rest] CC VP[NUM=?n, TENSE=?t, PER=?p, SUBCAT=?rest] 
+	#VP[NUM=?n, TENSE=?t, PER=?p] -> V[NUM=?n, TENSE=?t, PER=?p] PP  
+
+	VP[NUM=?n, PER=?p, SUBCAT=?rest] -> VP[NUM=?n, PER=?p, SUBCAT=[HEAD=?arg, TAIL=?rest]] ARG[CAT=?arg]
+	VP[NUM=?n, PER=?p, SUBCAT=?args] -> V[NUM=?n, PER=?p, SUBCAT=?args]
 	AP -> RB JJ | JJ
 	Nominal[NUM=?n] -> Nominal[NUM=?n] Noun[NUM=?n] | Noun[NUM=?n] 
 	PP -> IN NP
 
-	ARG[CAT=np] -> NP
-	ARG[CAT=vp] -> VP
+	ARG[NUM=?n, PER=?p, CAT=np] -> NP
+	ARG[NUM=?n, PER=?p, CAT=vp] -> VP
+	ARG[NUM=?n, PER=?p, CAT=pp] -> PP
+	ARG[NUM=?n, PER=?p, CAT=ap] -> AP
+
 	# Words
+
+	V[NUM=sg, PER=3, SUBCAT=nil] -> 'barks' | 'lauhgs'
+	V[NUM=sg, PER=3, SUBCAT=[HEAD=np, TAIL=[HEAD=pp, TAIL=nil]]] -> 'puts' | 'eats' | 'drinks'
+	V[NUM=sg, PER=3, SUBCAT=[HEAD=np, TAIL=[HEAD=np, TAIL=nil]]] -> 'feeds' | 'thinks'
+	V[NUM=sg, PER=3, SUBCAT=[HEAD=np, TAIL=[HEAD=vp, TAIL=nil]]] -> 'thinks'
 	
-	V[NUM=sg, PER=3] -> 'barks' | 'laughs' | 'eats' | 'feeds' | 'thinks' | 'drinks' | 'does'
+	# V[NUM=sg, PER=3] -> 'barks' | 'laughs' | 'eats' | 'feeds' | 'thinks' | 'drinks' | 'does'
  	V[TENSE=past] -> 'barked' | 'laughed' | 'ate' | 'fed' | 'thought' | 'drank' | 'did'
  	V[TENSE=prespart] -> 'barking' | 'laughing' | 'eating' | 'feeding' | 'thinking' | 'drinking' | 'doing'
- 	V[NUM=pl] -> 'bark' | 'laugh' | 'eat' | 'feed' | 'think' | 'drink' | 'do'
+ 	V[NUM=pl, CAT=trans] -> 'bark' | 'laugh' | 'eat' | 'feed' | 'think' | 'drink' | 'do'
  	Auxiliary[NUM=sg, PER=3] -> 'does'
  	Auxiliary[NUM=pl] -> 'do' | 'did'
  	ProperNoun[NUM=sg, PER=3] -> 'Gromit' | 'Wallace'
@@ -53,6 +73,9 @@ when Gromit barks Wallace feeds Gromit
 when does Wallace eat cheese
 """
 
+to_test = 
+"""when Gromit barks Wallace feeds Gromit"""
+
 invalid = """\
 Gromit bark
 when do Gromit eat cheese
@@ -60,9 +83,14 @@ Gromit barks the kitchen
 """
 
 def main():
+	print """\
+	##############################################
+	#         Begin                              #
+	##############################################
+	"""
 	g = Grammar(cfg_str)
-	g.parse_and_print(text)
-	g.parse_and_print(invalid, True)
+	g.parse_and_print(to_test, True)
+	#g.parse_and_print(invalid)
 
 if __name__ == '__main__':
 	main()
